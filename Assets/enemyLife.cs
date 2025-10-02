771d5f32d08e57d272a4c34b034a22e1baa1363e
Jordani12 <ojordani59@gmail.com>
2025-10-02 12:22:53 -0300

    Inimigos arrumados

--- Assets/enemyLife.cs
+++ Assets/enemyLife.cs
@@ -16,22 +16,24 @@ public class enemyLife : MonoBehaviour
     {
         actualLife = maxLife;
         anim = FindObjectOfType<Main_Animation>();
+        if (anim == null) Debug.Log("Null");
         if (actualLife >= maxLife / 4) shine_part.Stop();
     }
 
     public void TakeDamage(int value)
     {
         actualLife -= value;
-        anim.X_anim();
         if (actualLife <= 0) {
             actualLife = 0;
             if (gameObject.transform.parent != null) {//caso o componente life n�o esteja no objeto que queira destruir em si
-                Transform parent = gameObject.transform.parent; Die(parent.gameObject);
+                Transform parent = gameObject.transform.parent;
+                Die(parent.gameObject);
             }
             else
                 Die(gameObject);
         }
         shines(can_finalizate());
+        anim.X_anim();
     }
 
     private void Die(GameObject objeto) {
