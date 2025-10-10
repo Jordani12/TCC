027547d48bcce96a7d3ef4f158b6be979f8c0fc7
Jordani12 <ojordani59@gmail.com>
2025-10-10 04:02:20 -0300

    Arrumando dash tut

--- Assets/Scripts/Player/Mecanic/Gold/Gold.cs
+++ Assets/Scripts/Player/Mecanic/Gold/Gold.cs
@@ -3,24 +3,17 @@ using System.Collections;
 
 public class Gold : MonoBehaviour
 {
-    //referencias privadas
-    private GameObject player;
     private GunController controller;
 
-    private WaitForSeconds tempoAnimacao = new WaitForSeconds(0.5f);
-
-    //calculos privados
-    private int upgrade_per_gun;
-
-    private void Start(){
-        player = GameObject.FindGameObjectWithTag("Player");
-        controller = player.GetComponent<GunController>();
+    private void Awake()
+    {
+        controller = FindObjectOfType<GunController>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         PlayerStateManager player = other.GetComponent<PlayerStateManager>();
-        if(player != null){SeparateGuns();}
+        if(player != null) { SeparateGuns(); }
     }
 
     private void SeparateGuns()
@@ -31,14 +24,14 @@ public class Gold : MonoBehaviour
 
     private IEnumerator GunImprovement(Gun armaAtual)
     {
-        upgrade_per_gun = armaAtual.getGun.damage / 2;
+        int upgrade_per_gun = armaAtual.getGun.damage / 2;
         armaAtual.getGun.damage += upgrade_per_gun;
 
-        upgrade_per_gun = armaAtual.getGun.maximumAmmo / 4;
-        armaAtual.getGun.maximumAmmo += upgrade_per_gun;
+        int upgrade_per_gun2 = armaAtual.getGun.maximumAmmo / 4;
+        armaAtual.getGun.maximumAmmo += upgrade_per_gun2;
 
         //animação
-        yield return tempoAnimacao;
+        yield return new WaitForSeconds(0.5f);
 
         Destroy(gameObject);
     }
